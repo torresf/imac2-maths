729bc725d18b1d2c7d3cfd265ec42bbe1dcba391
torresf <torres.florian1@gmail.com>
2018-12-07 17:04:32 +0100

    TD10

--- td10/ex1.cpp
+++ td10/ex1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include <Eigen/QR>
 
 double evalPolynomial(const Eigen::VectorXd &p, const double &x) {
 	double res = 0;
@@ -23,16 +24,46 @@ Eigen::VectorXd polynomialFromRoots(const Eigen::VectorXd &roots) {
 	return p;
 }
 
+Eigen::VectorXd solvePolynomialRoots(const Eigen::VectorXd &p, const unsigned int nbIter) {
+	// Construction de la matrice compagnon
+	Eigen::MatrixXd c = Eigen::MatrixXd::Zero(p.rows()-1, p.rows()-1);
+	for (int i = 0; i < c.rows(); ++i)
+	{
+		for (int j = 0; j < c.cols(); ++j)
+		{
+			if (j == i-1) 
+				c(i,j) = 1;
+			if (j == c.rows()-1) 
+				c(i,j) = -p(i);
+		}
+	}
+
+	std::cout << "Compagnon : " << std::endl << c << std::endl;
+
+	for (unsigned int i = 0; i < nbIter; ++i)
+	{
+		Eigen::HouseholderQR<Eigen::MatrixXd> qr(c);
+		Eigen::MatrixXd Q = qr.householderQ();
+		Eigen::MatrixXd R = qr.matrixQR().triangularView<Eigen::Upper>();
+
+		c = R * Q;
+	}
+
+	return c.diagonal();
+}
+
 int main()
 {
-	// Eigen::VectorXd p1(3);
-	// p1 << 1, 2, 3;
-	// std::cout << evalPolynomial(p1, 2) << std::endl;
+	Eigen::VectorXd p1(3);
+	p1 << 1, 2, 3;
+	std::cout << evalPolynomial(p1, 2) << std::endl;
 
-	Eigen::VectorXd r(3);
-	r << 2, 3, 4;
+	Eigen::VectorXd r(10);
+	r << 1, 1, 3000, 4, 5, 6, 7, 8, 1000, 10;
 	Eigen::VectorXd p2 = polynomialFromRoots(r);
 	std::cout << p2 << std::endl;
+
+	std::cout << solvePolynomialRoots(p2, 150) << std::endl;
 
 	return 0;
 }
